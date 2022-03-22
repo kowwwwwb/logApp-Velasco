2b329c4a7fd46bc5215fad7ff4cd18cfcd3af42c
kowwwwwb <201980020@psu.palawan.edu.ph>
2022-03-23 01:23:27 +0800

    done changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '86a188c1ba740ad7c7d2c10a16c3c229379dab4d',
+        'name' => 'kowwwwwb/logapp-velasco',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kowwwwwb/logapp-velasco' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '86a188c1ba740ad7c7d2c10a16c3c229379dab4d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
